f342c9c2863fca917ab69efccdf12b4d40b310a0
Aaron Eaton <aaron@channeleaton.com>
2011-03-01 17:12:29 -0600

    System recognizes all group types in application/views/home_view.php

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,17 +1,33 @@
-<?php
+<?php // Checks to see if the user is logged in ?>
+<?php if(!$this->ion_auth->logged_in()) : ?>
 
-if (!$this->ion_auth->logged_in())
-{
-    redirect('auth/login');
-}
-else
-{
-    if ($this->ion_auth->is_admin())
-    {
-        echo 'You are the admin';
-    }
-    else
-    {
-        echo 'You are not an admin';
-    }
-}
+<p>You are not logged in!</p>
+<p>Please <?php echo anchor('auth/login', 'login'); ?>.</p>
+
+<?php // If user is logged in and an administrator, show Admin stuff ?>
+<?php elseif($this->ion_auth->is_admin()) : ?>
+
+    <p>You are the admin.</p>
+
+<?php // If user is logged in and an applicant, show Applicant stuff ?>
+<?php elseif($this->ion_auth->is_group('applicants')) : ?>
+
+    <p>You are an applicant</p>
+
+<?php // If user is logged in and a recommender, show Recommendation stuff ?>
+<?php elseif($this->ion_auth->is_group('recommenders')) : ?>
+
+    <p>You are a recommender.</p>
+
+<?php // If user is logged in and a reviewer, show Review stuff ?>
+<?php elseif($this->ion_auth->is_group('reviewers')) : ?>
+
+    <p>You are a reviewer.</p>
+
+<?php endif; ?>
+
+<?php if($this->ion_auth->logged_in()) : ?>
+
+    <p><?php echo anchor('auth/logout', 'Logout'); ?></p>
+
+<?php endif; ?>
--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,7 @@
 </div><!-- End div 'container' -->
-
+<div id="footer">
+    <h3>Footer</h3>
+</div>
 <script type="text/javascript" charset="utf-8">
 
     $('input').click(function(){
