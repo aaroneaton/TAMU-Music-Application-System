475b88f4f20b041e1215bfd89e72e4478611a165
Aaron Eaton <aaron@channeleaton.com>
2011-03-01 14:36:16 -0600

    Added initial template files.
    Template is loaded in controller via:
    $data['main_content'] = '<insert_view_here>';
    $this->load->view('includes/template', $data);

---
+++ application/views/includes/footer.php
@@ -0,0 +1,10 @@
+<script type="text/javascript" charset="utf-8">
+
+    $('input').click(function(){
+        $(this).select();
+    })
+
+</script>
+
+</body>
+</html>
---
+++ application/views/includes/template.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('includes/header'); ?>
+
+<div id="main_content">
+<?php $this->load->view($main_content); ?>
+</div>
+
+<?php $this->load->view('includes/footer'); ?>
