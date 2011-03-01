b0341da44f3582b9761e98e2632830e07863932a
Aaron Eaton <aaron@channeleaton.com>
2011-03-01 11:10:03 -0600

    Added line to .gitignore for application/config/email.php to keep sensitive data out of the public repository
    Updated README to include first install instructions.
    
    Added a blank /install/application/config/database.php
    
    Added a blank /install/application/config/email.php

---
+++ install/application/config/email.php
@@ -0,0 +1,7 @@
+<?php if ( !defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['protocol'] = '';
+$config['smtp_host'] = '';
+$config['smtp_port'] = ;
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
