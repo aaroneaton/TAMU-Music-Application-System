30e4de156e23f0af70d922b557f0d875d74a8d1c
Aaron Eaton <aaron@channeleaton.com>
2011-03-02 14:41:00 -0600

    Created create_app create_form methods in apply.php. Also created apply/create_view.php.

--- application/controllers/apply.php
+++ application/controllers/apply.php
@@ -9,9 +9,30 @@ class Apply extends CI_Controller {
         $this->load->view('includes/template', $data);
     }
 
+    /*
+     * This method loads the first page of the application process.
+     * 
+     * On this page will be a video showing how to use the application system
+     * along with instructions.
+     */
     function create_app()
     {
         $data['main_content'] = 'apply/create_view';
+        $this->load->view('includes/template', $data);
+    }
+
+    /*
+     * The create_form() method in it's current state loads, validates, and
+     * passes application info to the method.
+     *
+     * This method will later be expanded in to many methods to guide the
+     * applicant step-by-step through the process.
+     */
+    function create_form()
+    {
+
+
+        $data['main_content'] = 'apply/create_form_view';
         $this->load->view('includes/template', $data);
     }
 
--- application/views/apply/create_view.php
+++ application/views/apply/create_view.php
@@ -1,1 +1,7 @@
-<?php echo 'Hello'; ?>
+<h2>Create a new application</h2>
+
+<?php // @todo - Bring in user's first name ?>
+
+    <?php echo anchor('apply/create_form', 'Start your Application'); ?>
+
+
