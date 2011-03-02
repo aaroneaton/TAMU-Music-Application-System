2fced7cd8ff10694dfa53b216b66017a482c0d3c
Aaron Eaton <aaron@channeleaton.com>
2011-03-02 11:46:20 -0600

    Added stylesheet links.
    Allows for different styles based on media type and Internet Explorer 8

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,7 +1,14 @@
 <html>
     <head>
         <title>Untitled</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css" type="text/css" />
+        
+        
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/blueprint/screen.css" type="text/css" media="screen, projection" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>css/blueprint/print.css" type="text/css" media="print" />
+        <!--[if lt IE 8]>
+            <link rel="stylesheet" href="<?php echo base_url(); ?>css/blueprint/ie.css" type="text/css" media="screen, projection" />
+        <![endif]-->
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js" type="text/javascript" charset="utf-8"></script>
     </head>
     <body>
